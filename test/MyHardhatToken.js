84ddecb9cf46dfe787f55ad5c51861156356f2df
HanzhiDeng <hanzhi100@outlook.com>
2022-05-23 15:35:46 +0800

    test/add-transaction-token-transfer

--- test/MyHardhatToken.js
+++ test/MyHardhatToken.js
@@ -10,7 +10,7 @@ describe("MyToken", function () {
   let addr2;
 
   beforeEach(async function () {
-    [owner] = await ethers.getSigners();
+    [owner, addr1, addr2] = await ethers.getSigners();
     Token = await ethers.getContractFactory("MyHardhatToken");
     TokenV2 = await ethers.getContractFactory("MyHardhatToken");
     hardhatToken = await upgrades.deployProxy(Token, { kind: "uups" });
@@ -20,6 +20,13 @@ describe("MyToken", function () {
     it("Should assign the total supply of tokens to the owner", async function () {
       const ownerBalance = await hardhatToken.balanceOf(owner.address);
       expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transaction", function () {
+    it("Should transfer tokens between accounts", async function () {
+      await hardhatToken.transfer(addr1.address, 50);
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
     });
   });
 
