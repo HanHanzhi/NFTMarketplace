624d7bed8a07fd102229380e27863fc80cb6f690
Hanzhi <Hanzhi100@outlook.com>
2022-06-20 17:25:25 +0800

    add nft contract 2

---
+++ contracts/NftMarket.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
