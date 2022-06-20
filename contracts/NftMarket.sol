2db886bf0b41f2b044c9d661c2bb056745ca1260
Hanzhi <Hanzhi100@outlook.com>
2022-06-20 18:32:37 +0800

    add NFTmarket

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,8 +1,49 @@
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./MyHardhatToken.sol";
+import "./NFTCollectible.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "hardhat/console.sol"; //For debugging only
 
+contract NFTMarket{
+    struct Auction {
+        
+        address addressNFTCollection; // Address of the ERC721 NFT Collection contract
+        address addressPaymentToken; // Address of the ERC20 Payment Token contract
+        uint256 nftId; // NFT Id
+        address creator; // Creator of the Auction
+        address payable currentBidOwner; // Address of the highest bider
+        uint256 currentBidPrice; // Current highest bid for the auction
+        
+    }
+    Auction[] private allAuctions;
+    
+    function createAuction (
+        address _addressNFTCollection,
+        address _addressPaymentToken,
+        uint256 _nftId,
+        uint256 _initialBid
+
+    ) external returns (uint256) {
+        NFTCollectible nftCollection = NFTCollectible(_addressNFTCollection);
+        require(
+            nftCollection.ownerOf(_nftId) == msg.sender,
+            "Caller is not the owner of the NFT"
+        );
+        address payable currentBidOwner = payable(address(0));
+        // Create new Auction object
+        Auction memory newAuction = Auction({
+            
+            addressNFTCollection: _addressNFTCollection,
+            addressPaymentToken: _addressPaymentToken,
+            nftId: _nftId,
+            creator: msg.sender,
+            currentBidOwner: currentBidOwner,
+            currentBidPrice: _initialBid
+            
+        });
+        allAuctions.push(newAuction);
+        return 0;
+    }
+    
+}
---
+++ scripts/deployNftMarket.js
@@ -0,0 +1,19 @@
+const { utils } = require("ethers");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Marketplace = await ethers.getContractFactory("NFTMarket");
+  const marketplace = await Marketplace.deploy();
+
+  console.log("Marketplace address:", marketplace.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
