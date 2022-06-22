05749cb7e81901ea5ebb27fa1775e2838c17c19f
Hanzhi <Hanzhi100@outlook.com>
2022-06-22 16:16:46 +0800

    minor fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,30 +7,22 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-etherscan");
-
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "UTtJiBT_TnuDzEZyEW43oSikFpy_6_Tw";
-
-// Replace this private key with your Ropsten account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
-const ETHERSCAN_API_KEY = "MEGXFEK634F7EMDXWVYFXMNM5HIWHMSKEU";
-
-const ROPSTEN_PRIVATE_KEY =
-  "0543cd13d1b272a550ab23c0dfc7aa3d3643d3814b4e4fc5575395a6d16f8b41";
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.10",
   networks: {
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${ROPSTEN_PRIVATE_KEY}`],
+      url: process.env.ROPSTEN_ALCHEMY_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    goerli: {
+      url: process.env.GOERLI_ALCHEMY_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
   etherscan: {
     // API key for Etherscan
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
